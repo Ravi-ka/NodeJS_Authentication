b2823a966a9965fcbc7864fc02b63cf2fd3171a8
Ravisankar.Ka <ka.ravisankar@gmail.com>
2024-02-23 22:20:02 +0530

    Sessions

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import path from "path";
 import dotenv from "dotenv";
 import ejs from "ejs";
 import { connectToMongoDB } from "./config/dbConnection.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const server = express();
 const configPath = path.resolve("uat.env");
@@ -10,6 +12,21 @@ dotenv.config({ path: configPath });
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
+
+server.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URL,
+      collectionName: "sessions",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 server.set("view engine", "ejs");
 server.set(
